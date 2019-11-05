c510d43cd3519651017163b1b7c481ecc9e27a60
zhuny <zhuny@kaist.ac.kr>
2019-11-06 00:47:37 +0900

    detail controller get rev list

--- archive/controller.py
+++ archive/controller.py
@@ -1,3 +1,4 @@
+import datetime
 from operator import attrgetter
 
 from flask import current_app
@@ -35,5 +36,21 @@ class PageController:
         ).fetch(1)
         for row in query:
             return row
+
+    @classmethod
+    def get_rev_list(cls, page_id):
+        query = current_app.datastore.query(
+            namespace="Archive",
+            kind="WorkResult",
+            filters=[('key', '=', page_id)],
+            order=['-created_at'],
+            projection=['created_at']
+        )
+        result = list(query.fetch())
+        if result:
+            result.pop(0)  # remove first one if exists
+        for row in result:
+            row['created_at'] = datetime.datetime.fromtimestamp(row['created_at']/1000000)
+        return result
 
 
