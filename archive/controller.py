14509dce454bb898ecb25027189336e369c2721d
zhuny <zhuny@kaist.ac.kr>
2019-11-05 22:42:12 +0900

    controller

--- archive/controller.py
+++ archive/controller.py
@@ -1,7 +1,19 @@
+from operator import attrgetter
+
+from flask import current_app
 
 
 class PageController:
     # define classmethod to use controller
-    pass
+
+    @classmethod
+    def get_list(cls):
+        query = current_app.datastore.query(
+            namespace="Archive",
+            kind="Work"
+        )
+        result = list(query.fetch())
+        result.sort(key=attrgetter("id"))
+        return result
 
 
